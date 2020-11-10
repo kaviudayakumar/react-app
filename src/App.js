fabfcc87e685283c9546bcb17fc07f70f63bda8b
kaviudayakumar <kavi2390@gmail.com>
2020-11-10 13:26:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 // import Header from "./Components/Header";
 // import Footer from './Components/Footer';
-import Firebase from "firebase";
-import config from './config';
 import Body from './Components/Body.js';
 class App extends Component {
     constructor(props) {
         super(props);
-        //Firebase.initializeApp(config.firebase);
-
-        this.state = {
-            developers: []
-        }
     }
     render() {
         return (< div className="App" >
